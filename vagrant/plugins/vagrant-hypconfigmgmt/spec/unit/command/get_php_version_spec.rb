2e71fa11844f68bd17b738be48e1fa487e90a998
Rick van de Loo <rickvandeloo@gmail.com>
2018-02-05 14:35:36 +0100

    tabs are spaces in get_php_version_spec.rb

--- vagrant/plugins/vagrant-hypconfigmgmt/spec/unit/command/get_php_version_spec.rb
+++ vagrant/plugins/vagrant-hypconfigmgmt/spec/unit/command/get_php_version_spec.rb
@@ -25,14 +25,14 @@ describe VagrantHypconfigmgmt::Command do
 
     context "when PHP 5.5 is configured" do
       it "it notifies the user that PHP 5.5 will be used and returns the value" do
-	# check if the setting is prompted for and pretend it returns a "PHP 5.5" answer
+        # check if the setting is prompted for and pretend it returns a "PHP 5.5" answer
         expect(subject).to receive(:get_setting).with(
-	  env, AVAILABLE_PHP_VERSIONS, DEFAULT_PHP_VERSION,
-	  "Is this a PHP #{subject.get_options_string(AVAILABLE_PHP_VERSIONS)} Hypernode? [default #{DEFAULT_PHP_VERSION}]: "
-	).and_return("5.5")
+          env, AVAILABLE_PHP_VERSIONS, DEFAULT_PHP_VERSION,
+          "Is this a PHP #{subject.get_options_string(AVAILABLE_PHP_VERSIONS)} Hypernode? [default #{DEFAULT_PHP_VERSION}]: "
+        ).and_return("5.5")
         # check if the user is notified about the PHP version
         expect(ui).to receive(:info).once.with(/.*PHP 5.5*/)
-	# check if the function returns float 5.5 if a PHP 5.5 Vagrant is to be used
+        # check if the function returns float 5.5 if a PHP 5.5 Vagrant is to be used
         expect( subject.get_php_version(env) ).to eq(5.5)
       end
     end
@@ -40,14 +40,14 @@ describe VagrantHypconfigmgmt::Command do
 
     context "when PHP 5.6 is configured" do
       it "it notifies the user that PHP 5.6 will be used and returns the value" do
-	# check if the setting is prompted for and pretend it returns a "PHP 5.6" answer
+        # check if the setting is prompted for and pretend it returns a "PHP 5.6" answer
         expect(subject).to receive(:get_setting).with(
-	  env, AVAILABLE_PHP_VERSIONS, DEFAULT_PHP_VERSION,
-	  "Is this a PHP #{subject.get_options_string(AVAILABLE_PHP_VERSIONS)} Hypernode? [default #{DEFAULT_PHP_VERSION}]: "
-	).and_return("5.6")
+          env, AVAILABLE_PHP_VERSIONS, DEFAULT_PHP_VERSION,
+          "Is this a PHP #{subject.get_options_string(AVAILABLE_PHP_VERSIONS)} Hypernode? [default #{DEFAULT_PHP_VERSION}]: "
+        ).and_return("5.6")
         # check if the user is notified about the PHP version
         expect(ui).to receive(:info).once.with(/.*PHP 5.6*/)
-	# check if the function returns float 5.5 if a PHP 5.5 Vagrant is to be used
+        # check if the function returns float 5.5 if a PHP 5.5 Vagrant is to be used
         expect( subject.get_php_version(env) ).to eq(5.6)
       end
     end
@@ -55,14 +55,14 @@ describe VagrantHypconfigmgmt::Command do
 
     context "when PHP 7.0 is configured" do
       it "it notifies the user that PHP 7.0 will be used and returns the value" do
-	# check if the setting is prompted for and pretend it returns a "PHP 7.0" answer
+        # check if the setting is prompted for and pretend it returns a "PHP 7.0" answer
         expect(subject).to receive(:get_setting).with(
-	  env, AVAILABLE_PHP_VERSIONS, DEFAULT_PHP_VERSION,
-	  "Is this a PHP #{subject.get_options_string(AVAILABLE_PHP_VERSIONS)} Hypernode? [default #{DEFAULT_PHP_VERSION}]: "
-	).and_return("7.0")
+          env, AVAILABLE_PHP_VERSIONS, DEFAULT_PHP_VERSION,
+          "Is this a PHP #{subject.get_options_string(AVAILABLE_PHP_VERSIONS)} Hypernode? [default #{DEFAULT_PHP_VERSION}]: "
+        ).and_return("7.0")
         # check if the user is notified about the PHP version
         expect(ui).to receive(:info).once.with(/.*PHP 7.0*/)
-	# check if the function returns float 7.0 if a PHP 7.0 Vagrant is to be used
+        # check if the function returns float 7.0 if a PHP 7.0 Vagrant is to be used
         expect( subject.get_php_version(env) ).to eq(7.0)
       end
     end
@@ -70,14 +70,14 @@ describe VagrantHypconfigmgmt::Command do
 
     context "when PHP 7.1 is configured" do
       it "it notifies the user that PHP 7.1 will be used and returns the value" do
-	# check if the setting is prompted for and pretend it returns a "PHP 7.1" answer
+        # check if the setting is prompted for and pretend it returns a "PHP 7.1" answer
         expect(subject).to receive(:get_setting).with(
-	  env, AVAILABLE_PHP_VERSIONS, DEFAULT_PHP_VERSION,
-	  "Is this a PHP #{subject.get_options_string(AVAILABLE_PHP_VERSIONS)} Hypernode? [default #{DEFAULT_PHP_VERSION}]: "
-	).and_return("7.1")
+          env, AVAILABLE_PHP_VERSIONS, DEFAULT_PHP_VERSION,
+          "Is this a PHP #{subject.get_options_string(AVAILABLE_PHP_VERSIONS)} Hypernode? [default #{DEFAULT_PHP_VERSION}]: "
+        ).and_return("7.1")
         # check if the user is notified about the PHP version
         expect(ui).to receive(:info).once.with(/.*PHP 7.1*/)
-	# check if the function returns float 7.1 if a PHP 7.1 Vagrant is to be used
+        # check if the function returns float 7.1 if a PHP 7.1 Vagrant is to be used
         expect( subject.get_php_version(env) ).to eq(7.1)
       end
     end
